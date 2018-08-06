9197e481e9fa9905a530ac4e006927ae31185fac
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-06 00:44:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.size == 3 
     last_element = array.last
-    without_last = array.
+    without_last = array.remove(last_element)
     three_word_string = array.join(", ") << " and " << last_element
     return three_word_string
     
