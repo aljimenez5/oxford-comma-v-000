dbba4f70adaffc92bc0fbd97ef484628c3a083fe
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-06 00:24:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2 
     return array.join(" and ")
   elsif array.size == 3 
-    return array.join(",",",","and")
+    return array.join(", ",", "," and ")
   end
 end
