e0d23a2dc98b02df2d84b29b0706e8d80f1ba8d6
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-06 00:39:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,7 @@ def oxford_comma(array)
   elsif array.size == 3 
     last_element = array.last
     three_word_string = array.join(", ") << " and " << last_element
+    return three_word_string
     
     
   else
